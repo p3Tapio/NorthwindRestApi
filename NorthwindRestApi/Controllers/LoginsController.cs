df0e744bc2fc8d04697e92f3ab098cf7198c351e
p3Tapio <petri.laine@student.careeria.fi>
2020-03-13 22:32:36 +0200

    Login edits (get allista poistettu salasana, tee sama lastname-hakuun. Yms.)

--- NorthwindRestApi/Controllers/LoginsController.cs
+++ NorthwindRestApi/Controllers/LoginsController.cs
@@ -14,13 +14,25 @@ namespace NorthwindRestApi.Controllers
         [Route("")]
         public List<Logins> GetAllLogins()
         {
+   
             NorthwindContext context = new NorthwindContext();
-            var logs = context.Logins.ToList();
+            var logs = (from log in context.Logins
+                        select new Logins
+                        {
+                            LoginId = log.LoginId,
+                            Firstname = log.Firstname,
+                            Lastname = log.Lastname,
+                            Email = log.Email,
+                            Username = log.Username,
+                            AccesslevelId = log.AccesslevelId
+
+                        }).ToList();
+
             context.Dispose();
             return logs;
         }
         [HttpPost]
-        [Route("create")]
+        [Route("")]
         public string CreateNewLogin([FromBody] Logins login)
         {
 
@@ -29,7 +41,7 @@ namespace NorthwindRestApi.Controllers
             {
                 context.Logins.Add(login);
                 context.SaveChanges();
-                return "Login details for " + login.LoginId + " created.";
+                return "Login details for created.";
             }
             catch (Exception ex)
             {
@@ -41,6 +53,81 @@ namespace NorthwindRestApi.Controllers
             }
 
         }
-        // TODO: UPDATE + DELETE
+        [HttpGet]
+        [Route("{lastname}")]
+        public List<Logins> GetLogsByLastName(string lastname)
+        {
+            NorthwindContext context = new NorthwindContext();
+            var logins = (from x in context.Logins
+                          where x.Lastname.Contains(lastname)
+                          select x).ToList();
+
+            context.Dispose();
+            return logins;
+        }
+        [HttpPut]
+        [Route("{id}")]
+        public ActionResult UpdateLogin([FromBody] Logins loginInput, string id)
+        {
+            NorthwindContext context = new NorthwindContext();
+            try
+            {
+                Logins loginDb = context.Logins.Find(id);
+                if (loginDb != null)
+                {
+                    loginDb.LoginId = loginInput.LoginId;
+                    loginDb.Firstname = loginInput.Firstname;
+                    loginDb.Lastname = loginInput.Lastname;
+                    loginDb.Email = loginInput.Email;
+                    loginDb.Username = loginInput.Username;
+                    loginDb.Password = loginInput.Password;
+                    loginDb.AccesslevelId = loginInput.AccesslevelId;
+
+                    context.SaveChanges();
+                    return Ok("Login details for " + loginInput.LoginId + " updated.");
+                }
+                else
+                {
+                    return NotFound("Login details not found");
+                }
+            }
+            catch
+            {
+                return BadRequest();
+            }
+            finally
+            {
+                context.Dispose();
+            }
+        }
+        [HttpDelete]
+        [Route("delete/{id}")]
+        public ActionResult DeleteLogin(string id)
+        {
+            NorthwindContext context = new NorthwindContext();
+            try
+            {
+                Logins login = context.Logins.Find(id);
+                if (login != null)
+                {
+                    context.Logins.Remove(login);
+                    context.SaveChanges();
+                    return Ok("Customer '" + login.LoginId + "' deleted.");
+                }
+                else
+                {
+                    return NotFound("Customer '" + id + "' not found.");
+                }
+            }
+            catch
+            {
+                return BadRequest("Delete failed");
+            }
+            finally
+            {
+                context.Dispose();
+            }
+        }
+
     }
 }
